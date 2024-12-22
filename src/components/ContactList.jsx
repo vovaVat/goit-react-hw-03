d3afe0f22b86aaf7bd7db549996200bfa6c5db16
Volodymyr <vovavat97@gmail.com>
2024-12-22 22:56:54 +0200

    hz

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -5,7 +5,7 @@ export default function ContactList({ list, onDelete }) {
   return (
     <>
       <ul className={style.list}>
-        {list.map((elem, index) => {
+        {list.map((elem) => {
           return (
             <li key={elem.id} className={style.listElem}>
               <Contact elem={elem} onDelete={onDelete} />
